5578773d166baffe115d4a4301d95ba75f3b8706
Ian Chard <ian@chard.org>
2013-07-08 09:16:10 +0100

    first commit with the version from http://rainbow.chard.org/2011/12/13/nagboard-a-big-status-board-for-nagios/

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 nagboard
 ========
 
-A big status board for Nagios
+Introduction
+------------
+
+This is a little hack I wrote a while ago, inspired by a custom Flash app used
+at a previous place of work.  It consists of a bit of HTML that serves as a wrapper
+for a perl CGI script which gives you a scoreboard-like display.
+
+It's designed to be used on a big screen that is visible to all your ops staff.
+I display it in Firefox with the 'Full Fullscreen' plugin to get rid of all the
+browser decoration, so all you're left with is the scoreboard.
+
+Prerequisites
+-------------
+
+You need a working Nagios installation that uses a database as a back end.  For
+most installations this means using 'ndo2db'; on Debian-like systems, the package
+'ndoutils' will provide what you want.
+
+Installation
+------------
+
+Take 'nagboard.html' and 'nagboard.pl' and bung them in a directory on your Nagios
+server that Apache can see.  I use this directory and set of Apache directives:
+
+	<Directory "/var/www/nagboard/">
+		AllowOverride None
+		Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+		AddHandler cgi-script .pl
+	</Directory>
+
+You might need to tweak a couple of things in the perl script.  Nagboard will
+display some mrtg graphs when there are no alarms, so you'll have to substitute
+the URLs for those graphs.  If you don't want it to do that, just leave the URLs
+commented out.  The other thing is the database access details: 'localhost' will
+be fine if you're running this on the same server as your Nagios installation
+(recommended).  You'll just need to provide the username and password for your
+ndo2db database user.
+
+Other stuff
+-----------
+
+I'm a sysadmin, not a programmer, and the code might be very unpleasant.  There
+are lots of features that would be nice to have.  Feel free to request them or to
+send me patches, but please be nice.  Bitching about my code is pointless.
+
---
+++ nagboard.pl
@@ -0,0 +1,195 @@
+#!/usr/bin/perl
+#
+# A status scoreboard for Nagios.
+#
+# Ian Chard <ian@chard.org>  8/12/2011
+#
+
+use strict;
+use warnings;
+
+use DBI;
+use CGI::Pretty qw/:all/;
+use CGI::Cookie;
+use Data::Dumper;
+
+my @host_states=('ok', 'down', 'unreachable', 'unknown');
+my @service_states=('ok', 'warning', 'critical', 'unknown');
+
+# Obviously this bit will need to be customised for your mrtg (or whatever).
+# Graphs are displayed in pairs when there are no alarms.
+
+my %graphs=(
+#	'OO-RSL trunk'		=> 'https://my.server/mrtg/10.2.0.251_g1-day.png',
+#	'RSL public network'	=> 'https://my.server/mrtg/10.2.0.251-public-day.png',
+
+#	'Osney machine room temperature' => 'https://my.server/mrtg/oo-temp-day.png',
+#	'Osney line voltage'	=> 'https://my.server/mrtg/oo-voltage-day.png'
+);
+
+# Database connection details for the ndoutils database.
+
+my $dsn='DBI:mysql:database=ndoutils;host=localhost';
+my $dbuser='ndo';
+my $dbpass='ndo';
+
+my @down_hosts;
+my @t;
+my $alarms=0;
+my $graphpage=0;
+
+
+sub error($)
+{
+	print p({-class=>'down'}, $_[0]);
+	exit 1;
+}
+
+sub plural($) { return ($_[0]==1? '':'s') };
+
+sub is_blinking($)
+{
+	my $lastchange=$_[0];
+
+	return (time-$lastchange<300)? 'blinking':'moinking';
+}
+
+sub is_new($)
+{
+	my $lastchange=$_[0];
+
+	return (time-$lastchange<300)? 'new':'';
+}
+
+sub status_string($$$)
+{
+	my ($status, $lastchange, $flapping)=@_;
+	my $result;
+
+	my $now=time;
+
+	if($now%8<4)
+	{
+		my $age=$now-$lastchange;
+
+		if($age<60)
+		{
+			$result="+$age sec".plural($age);
+		}
+		elsif($age<3600)
+		{
+			$age=int($age/60);
+			$result="+$age min".plural($age);
+		}
+		elsif($age<172800)
+		{
+			$age=int($age/3600);
+			$result="+$age hr".plural($age);
+		}
+		else
+		{
+			$age=int($age/86400);
+			$result="+$age day".plural($age)."!";
+		}
+	}
+	else
+	{
+		$result=$flapping? 'flapping':$status;
+	}
+
+	return $result;
+}
+
+
+### PROGRAM ENTRY POINT ###
+
+my $dbh=DBI->connect($dsn, $dbuser, $dbpass, {'RaiseError' => 1});
+
+my $starttime_query=$dbh->prepare("select UNIX_TIMESTAMP(status_update_time) as status_update_time, is_currently_running from nagios_programstatus");
+
+my $hosts_query=$dbh->prepare("select display_name, current_state, output, problem_has_been_acknowledged, UNIX_TIMESTAMP(last_hard_state_change) as last_hard_state_change, current_check_attempt, s.max_check_attempts as max_check_attempts, s.scheduled_downtime_depth from nagios_objects, nagios_hosts as h, nagios_hoststatus as s where objecttype_id=1 and object_id=h.host_object_id and h.host_object_id=s.host_object_id");
+
+my $svcs_query=$dbh->prepare("select h.display_name as hostname, svcs.display_name, svcsstat.current_state, svcsstat.output as output, svcsstat.problem_has_been_acknowledged as problem_has_been_acknowledged, svcsstat.is_flapping as flapping, UNIX_TIMESTAMP(svcsstat.last_hard_state_change) as last_hard_state_change, svcsstat.current_check_attempt as current_check_attempt, svcsstat.max_check_attempts as max_check_attempts, svcsstat.scheduled_downtime_depth from nagios_objects, nagios_hosts as h, nagios_services as svcs, nagios_hoststatus as hoststat, nagios_servicestatus as svcsstat where objecttype_id=2 and object_id=svcs.service_object_id and svcs.service_object_id=svcsstat.service_object_id and svcs.host_object_id=h.host_object_id and svcs.host_object_id=hoststat.host_object_id");
+
+my $hostgroup_query=$dbh->prepare("select h.display_name, group_concat(name1 separator ' ') from nagios_hosts as h, nagios_hostgroup_members as memb, nagios_hostgroups as g, nagios_objects where objecttype_id=3 and object_id=hostgroup_object_id and memb.hostgroup_id=g.hostgroup_id and memb.host_object_id=h.host_object_id group by h.display_name");
+
+my $servicegroup_query=$dbh->prepare("select h.display_name, svcs.display_name, group_concat(name1 separator ' ') from nagios_hosts as h, nagios_servicegroup_members as memb, nagios_servicegroups as g, nagios_objects, nagios_services as svcs where objecttype_id=4 and object_id=servicegroup_object_id and memb.servicegroup_id=g.servicegroup_id and memb.service_object_id=svcs.service_object_id and svcs.host_object_id=h.host_object_id group by h.display_name, svcs.display_name");
+
+
+my %cookies=CGI::Cookie->fetch;
+my $cookie;
+
+if(!defined $cookies{'graphpage'})
+{
+	$graphpage=0;
+}
+else
+{
+	$graphpage=$cookies{'graphpage'}->value;
+}
+
+if(time%8==0)
+{
+	$graphpage=($graphpage+1)%(int((scalar keys %graphs)/2));
+	$cookie=CGI::Cookie->new(-name=>'graphpage', -value=>$graphpage);
+}
+
+print header(-expires=>'now', -cookie=>[$cookie]);
+
+$starttime_query->execute || error DBI::err.": ".$DBI::errstr;
+my $i=$starttime_query->fetchrow_hashref();
+
+error "Nagios is not running." if($$i{'is_currently_running'}!=1);
+
+my $updated_seconds_ago=time-$$i{'status_update_time'};
+error "Nagios not updated for $updated_seconds_ago seconds" if($updated_seconds_ago>60);
+
+$hosts_query->execute || error DBI::err.": ".$DBI::errstr;
+while(my $i=$hosts_query->fetchrow_hashref())
+{
+	if($$i{'current_state'}!=0 && $$i{'current_check_attempt'}==$$i{'max_check_attempts'})
+	{
+		if(!$$i{'problem_has_been_acknowledged'})
+		{
+			push @t, Tr(td({-name=>is_blinking($$i{'last_hard_state_change'}), -class=>is_new($$i{'last_hard_state_change'}).'down'}, [$$i{'display_name'},
+				status_string($host_states[$$i{'current_state'}], $$i{'last_hard_state_change'}, 0)]));
+			push @t, Tr(td({-class=>is_new($$i{'last_hard_state_change'}).'downoutput'}, [$$i{'output'}]));
+			$alarms++;
+		}
+
+		push @down_hosts, $$i{'display_name'};	
+	}
+}
+
+$svcs_query->execute || error DBI::err.": ".$DBI::errstr;
+while(my $i=$svcs_query->fetchrow_hashref())
+{
+	if($$i{'current_state'}!=0 && !grep(/^$$i{'hostname'}$/, @down_hosts) && $$i{'current_check_attempt'}==$$i{'max_check_attempts'})
+	{
+		if(!$$i{'problem_has_been_acknowledged'})
+		{
+			push @t, Tr(td({-name=>is_blinking($$i{'last_hard_state_change'}), -class=>is_new($$i{'last_hard_state_change'}).'notdown'}, [$$i{'hostname'}.':'.$$i{'display_name'},
+				status_string($service_states[$$i{'current_state'}], $$i{'last_hard_state_change'}, $$i{'flapping'})]));
+			push @t, Tr(td({-class=>is_new($$i{'last_hard_state_change'}).'output'}, [$$i{'output'}]));
+			$alarms++;
+		}
+	}
+}
+
+if($alarms)
+{
+	print table({-width=>'100%'}, @t);
+
+	my @time=localtime;
+	print div({-id=>'statusline'}, "$alarms alarm".plural($alarms));
+}
+else
+{
+	for($i=$graphpage*2; $i<=($graphpage*2)+1; $i++)
+	{
+		my $graph=(keys %graphs)[$i];
+
+		print p({-class=>'graphtext'}, $graph);
+		print img({-src=>$graphs{$graph}, -width=>'100%'});
+	}
+}
